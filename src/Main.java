dfb2379e4c0a7541d5a85be1a3290077da11cf13
Qasim <qasim.wardak2020@gmail.com>
2021-01-27 17:49:53 -0500

    Solar System Deployed

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        SolarSystem FactorX =new SolarSystem();
+
+        FactorX.sun = 1;
+        FactorX.moons = 3;
+        FactorX.planets = 2;
+
     }
 }
